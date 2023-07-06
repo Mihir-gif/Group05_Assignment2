5cc0975bbfea8df8269dd93b166d53463c6e40e2
Mihir <68594964+Mihir-gif@users.noreply.github.com>
2023-07-06 13:21:24 -0400

    FINAL COMMIT

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-// select the featured image and the thumbnail images
+//Here we select the featured and thumbnail images
 const featuredImage = document.querySelector('#gallery figure img');
 const thumbnailImages = document.querySelectorAll('#gallery ul li img');
 
-// set the featured image to the first image in the list
+// Here we set the featured image to appear first in the list
 featuredImage.src = thumbnailImages[0].src.replace('-small.jpg', '-large.jpg');
 featuredImage.alt = thumbnailImages[0].alt;
-
-featuredImage.width = 1200;
-featuredImage.height = 800;
-
 featuredImage.parentElement.querySelector('figcaption').textContent = thumbnailImages[0].alt;
 
-// user can click on the thumbnail photo 
-thumbnailImages.forEach(thumbnail => {
-  thumbnail.addEventListener('click', function() {
-    // update the featured image with the clicked thumbnail
-    featuredImage.src = this.src.replace('-small.jpg', '-large.jpg');
-    featuredImage.alt = this.alt;
-    featuredImage.parentElement.querySelector('figcaption').textContent = this.alt;
-  });
+// Using this function user can select the thumbnail image
+thumbnailImages.forEach(thumbnail =>
+{
+    thumbnail.addEventListener('click', function() 
+    {
+        // Here we update the featured image with the new click thumbnail image
+        featuredImage.src = this.src.replace('-small.jpg', '-large.jpg');
+        featuredImage.alt = this.alt;
+        featuredImage.parentElement.querySelector('figcaption').textContent = this.alt;
+    });
 });
