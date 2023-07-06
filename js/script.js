589df92cc2f403704501785288af46275340977d
Harshil52 <122923188+Harshil52@users.noreply.github.com>
2023-07-06 13:07:33 -0400

    Html Css updated

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const thumbnailImages = document.querySelectorAll('#gallery ul li img');
 // set the featured image to the first image in the list
 featuredImage.src = thumbnailImages[0].src.replace('-small.jpg', '-large.jpg');
 featuredImage.alt = thumbnailImages[0].alt;
+
 featuredImage.width = 1200;
 featuredImage.height = 800;
+
 featuredImage.parentElement.querySelector('figcaption').textContent = thumbnailImages[0].alt;
 
 // user can click on the thumbnail photo 
